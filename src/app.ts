0dcea181af1a66bdf27cb9c0ae3a583f1b9a3d83
Sumukha <sumukahegde007@gmail.com>
2024-05-25 23:45:33 +0530

    rendering the first form

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,22 @@
-console.log("hello");
-console.log("hhh");
+class ProjectList {
+  hostElement: HTMLTemplateElement;
+  projectElement: HTMLTemplateElement;
+  element: HTMLFormElement;
+  constructor() {
+    this.projectElement = document.querySelector(
+      "#project-input"
+    ) as HTMLTemplateElement;
+    this.hostElement = document.querySelector("#app")! as HTMLTemplateElement;
+
+    const importedNode = document.importNode(this.projectElement.content, true);
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const prjectInput = new ProjectList();
