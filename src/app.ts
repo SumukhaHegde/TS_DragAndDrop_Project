44d1782a50b9387b341ba232ff9f976558e84c24
Sumukha <sumukahegde007@gmail.com>
2024-05-26 23:11:12 +0530

    form submission code

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,41 @@ class ProjectList {
   hostElement: HTMLTemplateElement;
   projectElement: HTMLTemplateElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  personInputElement: HTMLInputElement;
+
+  descriptionInputElement: HTMLInputElement;
+
   constructor() {
     this.projectElement = document.querySelector(
       "#project-input"
     ) as HTMLTemplateElement;
     this.hostElement = document.querySelector("#app")! as HTMLTemplateElement;
+    this.titleInputElement = document.getElementById(
+      "title"
+    )! as HTMLInputElement;
+    this.descriptionInputElement = document.getElementById(
+      "description"
+    )! as HTMLInputElement;
+
+    this.personInputElement = document.getElementById(
+      "people"
+    )! as HTMLInputElement;
 
     const importedNode = document.importNode(this.projectElement.content, true);
     this.element = importedNode.firstElementChild as HTMLFormElement;
     this.element.id = "user-input";
 
     this.attach();
+    this.configureForm();
+  }
+
+  private handleSubmit(e: any) {
+    e.preventDefalut();
+  }
+
+  private configureForm() {
+    document.addEventListener("submit", this.handleSubmit);
   }
 
   private attach() {
